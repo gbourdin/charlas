eafea2f9f03906cc0ccf6102c8db40e614f5b5ca
German Bourdin <german.bourdin@gmail.com>
2014-09-20 02:12:43 -0300

    Actualizado el build script

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 ipython nbconvert --to slides rrule-slides.ipynb --reveal-prefix static/reveal.js-2.6.2 --config slides_config.py --SlidesExporter.file_extension=html
+ipython nbconvert --to slides rrule-slides-lujan.ipynb --reveal-prefix static/reveal.js-2.6.2 --config slides_config.py --SlidesExporter.file_extension=html
+
