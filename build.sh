555873524f1df1eaaf44c804e4576e076cf3c36a
German Bourdin <german.bourdin@gmail.com>
2014-11-15 02:07:25 -0300

    Actualizado el build script y agregado un build de las slides para pycon

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 ipython nbconvert --to slides rrule-slides.ipynb --reveal-prefix static/reveal.js-2.6.2 --config slides_config.py --SlidesExporter.file_extension=html
 ipython nbconvert --to slides rrule-slides-lujan.ipynb --reveal-prefix static/reveal.js-2.6.2 --config slides_config.py --SlidesExporter.file_extension=html
+ipython nbconvert --to slides rrule-slides-pycon.ipynb --reveal-prefix static/reveal.js-2.6.2 --config slides_config.py --SlidesExporter.file_extension=html
+
 
